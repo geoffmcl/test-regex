964ee0442b792b62eeded13344c11d5c0690e6f6
Geoff R. McLane <ubuntu@geoffair.info>
2018-05-09 20:05:08 +0200

    Show source ref for 'test-regex.cxx'

--- src/test-regex.cxx
+++ src/test-regex.cxx
@@ -1,3 +1,22 @@
+/*\
+ * regex-ex.cxx
+ *
+ * Copyright (c) 2018 - Geoff R. McLane
+ * Licence: GNU GPL version 2
+ *
+ * boost regex example
+ * from : https://stackoverflow.com/questions/21863049/what-is-wrong-with-this-usage-of-boost-regex-library
+ * where the poster reported "undefined reference to `boost::re_detail::perl_matcher..." and more...
+ * Modified the source a little to remove the 'regex gdreg(R"(g[a-z]+d)");' reference,
+ * but I do **not** get that problem - it seems to compile and run perfectly?????????
+ * My cmake find_package(Boost ...), with set(Boost_USE_STATIC_LIBS ON), finds my installed Boost
+ * -- Boost version: 1.58.0
+ * -- Found the following Boost libraries:
+ * --   regex
+ * -- *** Found Boost inc /usr/include, lib /usr/lib/x86_64-linux-gnu/libboost_regex.a
+ *
+\*/
+
 #include <iostream>
 #include <string>
 #include <boost/regex.hpp>
